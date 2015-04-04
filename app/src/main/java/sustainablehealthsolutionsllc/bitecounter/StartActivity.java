38f9c7c5630e7509d1be6ce5afe7347db4361975
Nghia (Jason) Vuong <vuo13001@byui.edu>
2015-04-04 13:35:56 -0600

    Update StartActivity.java

--- app/src/main/java/sustainablehealthsolutionsllc/bitecounter/StartActivity.java
+++ app/src/main/java/sustainablehealthsolutionsllc/bitecounter/StartActivity.java
@@ -1,9 +1,12 @@
 package sustainablehealthsolutionsllc.bitecounter;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class StartActivity extends ActionBarActivity {
@@ -12,6 +15,8 @@ public class StartActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        checkFirstRun();
+        addListenerImageButton();
     }
 
 
@@ -35,5 +40,36 @@ public class StartActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addListenerImageButton(){
+        Button button = (Button) findViewById(R.id.start_button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, BiteCounter.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void checkFirstRun() {
+        boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("isFirstRun", true);
+        if (isFirstRun){
+            // Place your dialog code here to display the dialog
+
+            getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                    .edit()
+                    .putBoolean("isFirstRun", false)
+                    .apply();
+        } else {
+            Intent intent = new Intent(StartActivity.this, BiteCounter.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        }
     }
 }
